22fa4d266c1e3121ca19076b9df3e239567d29b8
bupt_zzy <bupt_zzy>
2016-10-19 18:02:47 +0800

    he

--- read.py
+++ read.py
@@ -1,7 +1,6 @@
 #coding=utf-8
-f1 =open('condition_parameter.txt','r')
-a=0
-for line in f1:
-	if a==0:
-		print line
-		a+=1
+from selenium import webdriver
+driver=webdriver.Chrome()
+driver.get("http://www.baidu.com")
+
+print driver.title
