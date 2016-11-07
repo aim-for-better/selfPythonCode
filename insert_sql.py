e5a96c6d672797af8a05ce68e62cdc8bfc4f5bd8
aim-for-better <1253936411@qq.com>
2016-11-07 20:42:29 +0800

    update insert_sql.py

--- insert_sql.py
+++ insert_sql.py
@@ -1,4 +1,23 @@
 # -*-coding:utf-8-*-
 import pymysql
 
-conn=pymysql.connect
+host="localhost"
+user="root"
+passwd=""
+port=3306
+db="patentsviewdata"
+try:
+    conn=pymysql.Connect(host=host,user=user,passwd=passwd,port=port,db=db)
+
+    cur=None
+    if conn:
+        cur=conn.cursor()
+    print "connection database success"
+
+    cur.execute("select * from application limit5;")
+    result=cur.fetchone()
+    while result:
+        print result
+        result=cur.fetchone()
+except e:
+    print "Connection mysql database error! The error is %s",e
